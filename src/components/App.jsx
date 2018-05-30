8765e63bf124e09e864b0361c5b16efb18222345
Guest User <Guest@Epicodus-3D.local>
2018-05-30 11:54:31 -0700

    create new jsx files with basic functions for practice

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import Header from './Header';
 
 function App(){
-  return();
+  return(
+    <div>
+      <Header/>
+      <h3>1a</h3>
+      <h3>Ami and Bryce</h3>
+      <h3>We are hungry</h3>
+      <hr/>
+    </div>
+  );
 }
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,2 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
+
+reactDOM.render(
+  ,
+  document.getElementById('react-app-root')
+);
